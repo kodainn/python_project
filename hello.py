1b2fa3f09adaa08f92c6b0153900aef8157f36c1
TanakaKodai <kodai030513@gmail.com>
2023-03-31 20:37:15 +0900

    gitの練習も兼ねたflask演習1

---
+++ hello.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+bullets = ["箇条書き1",
+           "箇条書き2",
+           "箇条書き3",
+           "箇条書き4",
+           "箇条書き5",
+           "箇条書き6",
+           "箇条書き7",
+           "箇条書き8",
+           "箇条書き9"]
+
+@app.route("/")
+def hello():
+    return render_template("hello.html", bullets=bullets)
